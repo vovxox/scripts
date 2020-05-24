bd8311f0e6ffeba5bee6e1100dc67ab2a12aca10
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2020-05-24 11:20:26 -0400

    Update install-jenkins-docker.sh

--- install-jenkins-docker.sh
+++ install-jenkins-docker.sh
@@ -7,7 +7,7 @@ sudo apt-get install jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 
-# install docker
+# install docker // Ubuntu //https://docs.docker.com/engine/install/ubuntu/
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
