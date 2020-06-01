894aa4d1c23e9fdb1172bf097196ee82c0a8b1f1
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2020-06-01 16:10:13 -0400

    install java in amazon linux
    install java in amazon linux

---
+++ install-java.sh
@@ -0,0 +1,11 @@
+// check existing java version
+java -version
+
+// install java 8
+sudo yum install java-1.8.0
+
+// check which java version acting
+java -version
+
+// if its not the one you installed then run the below command to set the java version. 
+sudo sudo update-alternatives --config java
