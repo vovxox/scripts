a4a2e159d6636b8f7c9a2b03bd6429d557a241ea
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2020-10-23 11:00:09 -0400

    Update java-install.sh

--- java-install.sh
+++ java-install.sh
@@ -1,14 +1,28 @@
-// check existing java version
+### check existing java version
+
 java -version
 
-// install java 8
-sudo yum install java-1.8.0
+### Run below commands to install Java 11 on Amazon Linux:
 
-// check which java version acting
+sudo amazon-linux-extras install java-openjdk11
+
+### Run below commands to install Java 8 on Amazon Linux:
+
+sudo yum install java-1.8.0-openjdk
+
+sudo alternatives --config java
+
+### check which java version acting
+
 java -version
 
-// if its not the one you installed then run the below command to set the java version. 
+### if its not the one you installed then run the below command to set the java version. 
+
 sudo sudo update-alternatives --config java
 
-//if you want to uninstall existing java
+### if you want to uninstall existing java
+
 sudo yum remove java
+
+
+
